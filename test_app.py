c4c7ded1dd34320e23e3e09e3ede8b16300b01f6
kaisarabdans <57064199+kaisarabdans@users.noreply.github.com>
2021-03-15 22:22:02 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -25,8 +25,8 @@ class TestApp(unittest.TestCase):
 #        response = multiprocessing_test.multiprocessing_test()
 #        self.assertEqual(response, True)
 		
-    def test_02_rolly_113040087(self):
-        from Chapter02.Rolly113040087 import main	
+    def test_02_kaisar_1184093(self):
+        from Chapter02.Kaisar1184093 import main	
         response = 	main()
         self.assertEqual(response, True)
 		
