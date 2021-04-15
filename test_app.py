d7ea8c1755b7f8b90cab926864b310d21e52b485
unknown <wahyukurniasari88@gmail.com>
2021-04-16 05:58:05 +0700

    WahyuKurniaSariEnam1184001

--- test_app.py
+++ test_app.py
@@ -234,3 +234,12 @@ class TestApp(unittest.TestCase):
         from Chapter02.ParhanEnam1184042 import main
         response =  main()
         self.assertEqual(response, True)
+   
+    def test_06_WahyuKurniaSari_1184001(self):
+        from Chapter02.WahyuKurniaSariEnam1184001 import main
+        result=main()
+        self.assertEqual(result, True)
+        
+      
+
+
