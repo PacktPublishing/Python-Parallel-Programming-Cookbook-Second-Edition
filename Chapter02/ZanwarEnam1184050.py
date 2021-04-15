5f38dc3c3d89134bc2b667681d7603093e4ba95b
zanwar <zanwararif1331@gmail.com>
2021-04-15 11:39:18 +0700

    update fungsi

--- Chapter02/ZanwarEnam1184050.py
+++ Chapter02/ZanwarEnam1184050.py
@@ -3,7 +3,7 @@ import os
 import requests
 from time import ctime, sleep
 
-kali = 3
+kali = 1
 barr = Barrier(kali)
 file = "za"
 filename = os.path.join(os.path.dirname(__file__), file)
@@ -20,47 +20,31 @@ def randomapi():
         string = string+tulis+str(hasil)
     createfile(string)  
 
-def read():
-    x = open(filename+'2.txt', "r")
-    print(x.read())
-    x.close()
-
 def createfile(isi):
-       for i in range(kali):
-           print('Create File : '+file+str(i)+'.txt')
+       for i in range(3):
+           print('Create File : '+file+str(i)+'.txt \n')
            f = open(filename+str(i)+".txt", "w")
            f.write(str(isi))
-           sleep(2)
-           barr.wait()
-           print('Read File:'+file+str(i)+'.txt')
-           x = open(filename+str(i)+".txt", "r")
-           print(x.read())
-           x.close()
-           print('Create & Read reached the barrier at: %s \n' % (ctime()))
-           sleep(2)
-           barr.wait()
            f.close()
-
+           print('Read File: '+file+str(i)+'.txt')
+           x = open(filename+str(i)+".txt", "r")
+           print(x.read()+'\n')
+           x.close()
+           print('Create & Read '+file+str(i)+' reached the barrier at: %s \n' % (ctime()))
+           sleep(2)
+           barr.wait()
+           
 def run():
     randomapi()
     sleep(2)
-    print('Remove 0 reached the barrier at: %s \n' % (ctime()))
     barr.wait()
-    print('hapus file pertama!')
-    os.remove(filename+'0.txt')
-    sleep(2)
-    print('Remove 1 reached the barrier at: %s \n' % (ctime()))
-    barr.wait()
-    print('hapus file kedua!')
-    os.remove(filename+'1.txt')
-    print('Baca File za2.txt')
-    read()
+    print('All reached the barrier at: %s \n' % (ctime()))
 
 ### In[]:
 
 def main():
     threads = []
-    print('START!!!!')
+    print('START!')
     for i in range(kali):
         threads.append(Thread(target=run))
         threads[-1].start()
