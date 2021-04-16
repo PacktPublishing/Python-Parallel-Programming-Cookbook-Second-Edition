488edf41d01c2103d1c658b68786d73d0b39a6a3
hernandez2804 <56632278+hernandez2804@users.noreply.github.com>
2021-04-16 08:54:42 +0700

    update Tugas 6 Hernandez

--- test_app.py
+++ test_app.py
@@ -240,11 +240,13 @@ class TestApp(unittest.TestCase):
     #     response =  main()
     #     self.assertEqual(response, True)
 
-    def test_06_WahyuKurniaSari_1184001(self):
-        from Chapter02.WahyuKurniaSariEnam1184001 import main
-        result=main()
-<<<<<<< HEAD
-        self.assertEqual(result, True)
-=======
-        self.assertEqual(result, True)
->>>>>>> d44e0b07775ddf642cd4fa0af63714f34d54f927
+    #def test_06_WahyuKurniaSari_1184001(self):
+    #    from Chapter02.WahyuKurniaSariEnam1184001 import main
+    #    result=main()
+    #    self.assertEqual(result, True)
+
+    def test_06_nandez_1184014(self):
+         from Chapter02.IrfanHernandezEnam1184014 import main
+         response =  main()
+         self.assertEqual(response, True)
+
