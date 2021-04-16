e118e8a8ff1fcd553be3f882980ae9b1783e2584
Okky Yudistira <okkyyudistira08@gmail.com>
2021-04-16 11:38:18 +0700

    Perbaikan Okky 1184087

--- test_app.py
+++ test_app.py
@@ -283,9 +283,9 @@ class TestApp(unittest.TestCase):
     #     response =  main()
     #     self.assertEqual(response, True)
 
-<<<<<<< HEAD
+
     #def test_06_WahyuKurniaSari_1184001(self):
-=======
+
 
       # def test_06_WahyuKurniaSari_1184001(self):
     #     from Chapter02.WahyuKurniaSariEnam1184001 import main
@@ -305,18 +305,11 @@ class TestApp(unittest.TestCase):
     #     self.assertEqual(response, True)
 
 
->>>>>>> 053c7fd69c430b43a9756844987e50795a0d78f3
+
     #    from Chapter02.WahyuKurniaSariEnam1184001 import main
     #    result=main()
     #    self.assertEqual(result, True)
 
-<<<<<<< HEAD
-
-    def test_06_Okky_1184087(self):
-        from Chapter02.OkkyEnam1184087 import main
-        response = main()
-        self.assertEqual(response, True)
-=======
     #def test_06_ira_1184024(self):
     #    from Chapter02.IraEnam1184024 import main
     #    response =  main()
@@ -360,10 +353,10 @@ class TestApp(unittest.TestCase):
     #     response =  main()
     #     self.assertEqual(response, True)
         
-    def test_06_josuansef_1184091(self):
-         from Chapter02.JosuansefEnam_1184091 import main
-         response =  main()
-         self.assertEqual(response, True)
+    #def test_06_josuansef_1184091(self):
+    #     from Chapter02.JosuansefEnam_1184091 import main
+    #     response =  main()
+    #     self.assertEqual(response, True)
 
     def test_06_Okky_1184087(self):
         from Chapter02.OkkyEnam1184087 import main
