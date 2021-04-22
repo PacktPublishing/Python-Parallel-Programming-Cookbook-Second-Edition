8d43c488fd37f16bd37894de44eedb1dfde8d4af
Vicky Safira <safiravicky25@gmail.com>
2021-04-22 15:48:28 +0700

    VickySafTujuh1184037

---
+++ Chapter02/VickyTujuh1184037.py
@@ -0,0 +1,71 @@
+from threading import Thread
+from queue import Queue
+import os 
+import requests
+
+queue = Queue()
+ayo = []
+apiurl ='https://kodepos-2d475.firebaseio.com/list_kotakab/p9.json?print=pretty'
+response = requests.get(apiurl)
+html=response.json()
+
+class vickysaf(Thread):
+    def __init__(self, namathread, filename):
+        Thread.__init__(self)
+        self.filename = os.path.join(os.path.dirname(__file__), filename)
+        self.namathread = namathread
+        
+    def mainapi(self):
+        string = "kertas:"
+        for i in range(len(html["paper"])):
+            sini = html["paper"][i]["buku"]
+            nomor = "\n"+str(i)+". "
+            string = string + nomor + sini
+            lagi.append(sini)
+            queue.put(lagi)
+            print(str(i)+". %s yg di append" % (sini))
+        self.createfile(string)
+        self.kebelakang()
+            
+    def run(self):
+        print("Dimulai")
+        self.mainapi()
+        print("Selesai")
+        
+    def createfile(self, isi):
+        f = open(self.filename, "w")
+        f.write(str(isi))
+        f.close()
+    
+    def read(self):
+        x = open(self.filename, "r")
+        print(x.read())
+        x.close() 
+        
+    def belakang(self):
+        kebelakang = queue.get()
+        dur = len(kebelakang)
+        while dur > 0:
+            print("hasil pop")
+            dur = dur -1
+        self.read()
+        queue.task_done()
+    
+    
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
